17e566856a4da014214a51aa9168edef0a813555
GraceEbert <63425481+GraceEbert@users.noreply.github.com>
2020-04-17 03:38:35 +0000

    MySecondProject Week 13

---
+++ MySecondProject/App copy 2.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, TextInput, View } from 'react-native';
+
+ function MyApp() {
+  return ( 
+    <View>
+
+      <Text>
+       {"\n\n\n\n\n\n\n"} 
+      Hello I am a student in CIS340! {"\n"};
+      </Text>
+      </View>
+  );
+}
+
+
---
+++ MySecondProject/App copy.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text } from 'react-native';
+
+export default function MyApp() {
+  const pet ="Cat"; 
+  return (
+
+      <Text>
+       {"\n\n\n\n\n\n\n"} 
+      Hello I am a student in CIS340! {"\n"};
+      I have a {pet}!
+      </Text>
+  );
+}
+
+
